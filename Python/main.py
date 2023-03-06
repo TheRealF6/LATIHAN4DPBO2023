a325f5e49170b6f1e89a9d923abbb17b1014b554
Muhammad Fakhri Fadhlurrahman <119662753+TheRealF6@users.noreply.github.com>
2023-03-06 23:46:11 +0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     dosen = Dosen("3225022512830002", "Yudhayana Argawijaya", 'L', "1501234", "FPMIPA", "S3", "AI")
     sivikMhs = SivitasAkademik("UPI", "ardiansyah@upi.edu")
     sivikDsn = SivitasAkademik("UPI", "yana_yudha@upi.edu")
-    prodi = ProgramStudi("Ilmu Komputer", "IK100")
+    prodi = ProgramStudi("Ilmu Komputer", "IK100MIPA")
     course = Course("Kecerdasan Buatan")
     course.setNama_matakuliah("Machine Learning")
     course.setNama_matakuliah("Internet of Things")
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     print("Pendidikan Terakhir : ", dosen.getPend_terakhir())
     print("Bidang Keahlian : ", dosen.getKeahlian())
     print("Program Studi : ", prodi.getNama_prodi())
-    print("Kode Course : ", prodi.getKode())
+    print("Kode : ", prodi.getKode())
     i = 1;
     for course_name in course.getNama_matakuliah():
         print(f"Nama Course ke-{i}: {course_name}")
